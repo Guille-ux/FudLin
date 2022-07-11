1c0050eaf2803d9f097b2f30839aff6da5531807
Guille-ux <104579306+Guille-ux@users.noreply.github.com>
2022-07-11 16:13:19 +0200

    creted fulin.py and finished

---
+++ FudLin.py
@@ -0,0 +1,26 @@
+import os
+import sys
+import time
+import base64
+
+print(" ______________ _________________   ___       ___ _________       ")
+print("|   _    |     Y     |      _  . \ |   |     |   |     _    \     ")
+print("|.  1____|.    |     |.     |     \|.  |     |.  |.    |     |    ")
+print("|.   __) |.    |     |.     |      |.  |     |.  |.    |     |    ")
+print("|:  |    |:    1     |:     1      |:  |_____|:  |:    |     |    ")
+print("|::.|    |::.. .     |::.. .      /|::.. .   |::.|::.  |     |    ")
+print(" ---      ----------- -----------   --------- --- -----------  1.0")
+print("                                 by guille-exploit@protonmail.com ")
+print("    Select a option to begin. ")
+print("")
+print("   [ 1 ] Bashload (Bash) (NEW) ")
+print("   [ 2 ] NetBot (Python3) (NEW) ")
+print("   [ 3 ] exit")
+opt = input("[FUDWIN]--> ")
+os.chdir("payload")
+if opt == 1:
+  os.system("python3 bashloader.py")
+elif opt == 2:
+  os.system("python3 nbot.py")
+else:
+  sys.exit()
